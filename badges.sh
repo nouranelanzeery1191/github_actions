91636af7e8d7c62ef5c6b06f84905304f2a72647
nelanzeery <nouran.elanzeery@accenture.com>
2020-11-23 22:14:35 +0100

    added GHA workflow badge

--- badges.sh
+++ badges.sh
@@ -3,7 +3,7 @@
 Workflow_name=${1}
 Branch_name=${2}
 GitHub_Event=${3}
-Badge="![](https://github.com/OttoPaymentHub/barracuda_partner_accounting/workflows/'${Workflow_name}'/badge.svg?branch='${Branch_name}'&event='${GitHub_Event}')"
+Badge="![](https://github.com/OttoPaymentHub/barracuda_partner_accounting/workflows/${Workflow_name}/badge.svg?branch=${Branch_name}&event=${GitHub_Event})"
 
 Job_Status="${Workflow_name} | ${Branch_name} | ${GitHub_Event} | ${Badge}"
 
